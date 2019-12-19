1bfb4624940eb3863ae205f10cc98ad0fbbfb051
Hunter Werlla <werlla@amazon.com>
2019-12-19 14:25:01 -0800

    something kind of working

--- build-scripts/generateTelemetryJson.ts
+++ build-scripts/generateTelemetryJson.ts
@@ -6,45 +6,17 @@
 import { readFileSync } from 'fs-extra'
 import * as jsonParser from 'jsonc-parser'
 
-/*
-function isSorted(m: string[]): Boolean {
-    for (let iterator = 0; iterator < m.length - 1; iterator++) {
-        if (!m[iterator].localeCompare(m[iterator + 1])) {
-            console.log(`${m[iterator + 1]} and ${m[iterator]} are not in order`)
-
-            return false
-        }
-    }
-
-    return true
-}
-
-type Unit = 'none' | 'count'
-type MetadataType = 'string' | 'number'
-
-interface Metadata {
-    name: string,
-    metadataType: MetadataType
-    optional: boolean
-    allowedValues: any[]
-}
-
-interface Metric {
-    name: string,
-    unit: Unit,
-    metadata: Metadata[]
-}*/
-
 type MetricType = 'none' | 'count'
+type MetadataItemType = 'string'
 
-interface TypeDefinition {
+interface MetadataType {
     name: string
-    type: string
-    allowedValues: string[]
+    type: MetadataItemType
+    allowedValues?: string[]
     required: boolean
 }
 
-type MetricMetadataType = TypeDefinition | string
+type MetricMetadataType = MetadataType | string
 
 interface Metric {
     name: string
@@ -53,52 +25,17 @@ interface Metric {
 }
 
 interface MetricDefinitionRoot {
-    metadataTypes: TypeDefinition[]
+    metadataTypes: MetadataType[]
     metrics: Metric[]
 }
 
 const file = readFileSync('build-scripts/telemetrydefinitions.json', 'utf8')
 const errors: jsonParser.ParseError[] = []
 const telemetryJson = jsonParser.parse(file, errors) as MetricDefinitionRoot
+const globalMetadata = telemetryJson.metadataTypes
+const metrics = telemetryJson.metrics
 
-//const allowedMetadataTypes = telemetryJson.metadataTypes
-console.log(telemetryJson.metadataTypes)
-console.log(telemetryJson.metrics)
-
-telemetryJson.metadataTypes.forEach(item => console.log(item.name))
-
-/*
-const allMetadata = file.slice(0, file.findIndex((item: string) => item.trim() === 'metrics:')).filter((item: string) => item.trim() !== '')
-const allMetrics = file.slice(file.findIndex((item: string) => item.trim() === 'metrics:') + 1).filter((item: string) => item.trim() !== '')
-
-if (!isSorted(allMetrics)) {
-    console.log('telemetry definitions are not in alphabetical order!')
-    throw undefined
-}
-
-const metadata: Metadata[] = allMetadata.map((mtype) => {
-    const parts = mtype.split(',').map((x: string) => x.trim())
-
-    return {
-        name: parts[0],
-        metadataType: parts[1] as MetadataType,
-        optional: false,
-        allowedValues: parts.slice(3) as any[]
-    }
-})
-
-const metrics: Metric[] = allMetrics.map((metric) => {
-    const parts = metric.split(',').map((x: string) => x.trim())
-
-    return {
-        name: parts[0],
-        unit: parts[1] as Unit,
-        metadata: metadata
-    }
-})
-
-let output =
-`
+let output = `
 import { ext } from '../../shared/extensionGlobals'
 
 enum TelemetryType {
@@ -109,19 +46,72 @@ metrics.forEach((metric: Metric) => {
 
 output += '}\n\n'
 
-metrics.forEach((metric) => {
-    output +=
-`function record${metric.name.split('_').map(item => item.replace(item[0], item[0].toUpperCase())).join('')}(${metric.metadata.map(metric => metric.name).join(', ')})}){
+globalMetadata.forEach((metadata: MetadataType) => {
+    if ((metadata?.allowedValues?.length ?? 0) === 0) {
+        return
+    }
+    const values = metadata!.allowedValues!.map((item: string) => `"${item}"`).join(' | ')
+
+    output += `type ${metadata.name} = ${values}`
+
+    output += '\n'
+})
+
+output += '\n'
+
+metrics.forEach(metric => {
+    const metadata = metric.metadata.map(item => {
+        if (typeof item === 'string') {
+            const s = item as string
+            if (!s.startsWith('$')) {
+                console.log('you messed up son, you have to preface your references with the sigil "$"')
+                throw undefined
+            }
+            const foundMetadata = globalMetadata.find((candidate: MetadataType) => candidate.name === s.substring(1))
+            if (!foundMetadata) {
+                console.log('Come on you can not reference things that do not exist')
+                throw undefined
+            }
+
+            return foundMetadata
+        } else {
+            return item
+        }
+    })
+
+    const name = metric.name
+        .split('_')
+        .map(item => item.replace(item[0], item[0].toUpperCase()))
+        .join('')
+
+    const args = metadata
+        .map((m: MetadataType) => {
+            let t = m.name
+            if ((m?.allowedValues?.length ?? 0) === 0) {
+                t = m.type
+            }
+
+            return `${m.name}${m.required ? '' : '?'}: ${t},`
+        })
+        .join('\n    ')
+
+    output += `interface ${name} {
+    value?: number
+    ${args}
+}\n\n
+`
+    output += `function record${name}(args: ${name}){
     ext.record(
             {
                 name: TelemetryType.${metric.name.toUpperCase()},
-                value: 0,
-                unit: ${metric.unit},
-                metadata: [{${metric.metadata.map((metric) => `${metric.name}: ${metric.name}` )}}]
+                value: args.value ?? 1.0,
+                unit: ${metric.type},
+                metadata: [{\n                    ${metadata
+                    .map((m: MetadataType) => `${m.name}: args.${m.name}`)
+                    .join('\n                    ')}\n                }]
             }
-    ),
+    )
 }\n\n`
 })
 
 console.log(output)
-*/
